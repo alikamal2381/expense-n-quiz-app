810d9efd59a4a81f332a64df6df8ff4157e757aa
AliKamal <alikamal2381@gmail.com>
2020-09-06 20:37:17 +0500

    seven-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import main from "../src/images/main1.png";
 import Header from "./components/header/Header";
@@ -6,11 +6,30 @@ import ExpenseTrackerApp from "./components/exp_tracker/ExpenseTracker";
 import QuizApp from "./components/quiz_app/QuizApp";
 
 function App() {
+  const [rowId, setRowId] = useState(0);
+
+  useEffect(() => {
+    if (rowId === 0) {
+      console.log("Component loaded!");
+    } else {
+      console.log("Button was clicked!");
+    }
+  }, [rowId]);
+
   function clickExpenseTracker() {
+    setRowId(1);
+    //if (rowId === 1) {
+    console.log("expense");
     return <ExpenseTrackerApp />;
+    //}
   }
+
   function clickQuiz() {
+    setRowId(2);
+    //if (rowId === 2) {
+    console.log("quiz");
     return <QuizApp />;
+    //}
   }
 
   return (
@@ -18,16 +37,17 @@ function App() {
       <Header />
       <main className="container">
         <section className="hero container">
-          <h1>Expense Tracker & Quiz App</h1>
+          <h3>Expense Tracker & Quiz App</h3>
           <p>
             Bootcamp 2020 Project 7A: Rebuild the Expense Tracker App and Quiz
             App with React/TypeScript it should be a Progressive Web App (PWA)
             with Offline Capability and Push Notifications
           </p>
+          <p>Click: {rowId}</p>
           {/* <a href="#">Expense Tracker App</a> <a href="#">Quiz App</a> */}
-          <button onClick={clickExpenseTracker}>
-            Expense Tracker App
-          </button>{" "}
+          {/*    <button onClick={() => setRowId(1)}>Expense Tracker App</button>{" "}
+          <button onClick={() => setRowId(2)}>Quiz App</button> */}
+          <button onClick={clickExpenseTracker}>Expense Tracker App</button>{" "}
           <button onClick={clickQuiz}>Quiz App</button>
         </section>
         <div className="device">
